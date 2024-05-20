9247a4882a679e2edca1feb77b293d921ef600e4
tomii <dev.tomii03@gmail.com>
2024-05-20 19:10:24 -0300

    Background image added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,27 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import {Providers} from './provider'
+import { Providers } from "./provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Coliseum Web",
-  description: "NextJS + NextUI",
+    title: "Coliseum Web",
+    description: "NextJS + NextUI",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className="dark">
-      <body className={inter.className}>
-        <Providers>{children}</Providers>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" className="dark">
+            <body className={inter.className}>
+                <div className="img-bg">
+                    <Providers>{children}</Providers>
+                </div>
+            </body>
+        </html>
+    );
 }
