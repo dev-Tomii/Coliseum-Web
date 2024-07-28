5048f77254b20f6f21919fb9f7cc26f0d52361af
Tomii Medina <dev.tomii03@gmail.com>
2024-07-28 10:50:11 -0300

    deploy fix maybe

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./provider";
-import Navegation from "../components/navegation";
+import Navegation from "@/components/navegation";
 
 const inter = Inter({ subsets: ["latin"] });
 
