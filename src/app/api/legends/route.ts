64b9c5b5f761cd50054341de47f1d84f8d244c6d
Tomii Medina <dev.tomii03@gmail.com>
2024-08-06 04:05:21 -0300

    added api files

--- src/app/api/legends/route.ts
+++ src/app/api/legends/route.ts
@@ -1,6 +1,11 @@
-import { NextResponse } from 'next/server'
-import legends from './legends.json'
+import { NextResponse } from "next/server";
+import legends from "./legends.json";
 
 export async function GET() {
-    return NextResponse.json(legends) 
+    return NextResponse.json(legends);
+}
+
+export async function POST(request: Request) {
+    const data = await request.json();
+    return NextResponse.json(data);
 }
